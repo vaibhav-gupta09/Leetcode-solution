1fea5fe36ec6d8ab2b50ae83886c7e20ac4ba736
vaibhav gupta <96633424+vaibhav-gupta09@users.noreply.github.com>
2023-04-15 01:15:43 +0530

    Added solution - LeetHub

---
+++ Find Total Time Taken - GFG/find-total-time-taken.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            
+            int n=Integer.parseInt(in.readLine().trim());
+            int a[]=new int[n];
+            String s[]=in.readLine().trim().split(" ");
+            for(int i=0;i<n;i++){
+                a[i]=Integer.parseInt(s[i]);
+            }
+            s=in.readLine().trim().split(" ");
+            int time[]=new int[n];
+            for(int i=0;i<n;i++){
+                time[i]=Integer.parseInt(s[i]);
+            }
+            Solution ob=new Solution();
+            long ans=ob.totalTime(n,a,time);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static long totalTime(int n,int arr[],int time[])
+    {
+        long[]unlock = new long[n+1];
+        long curr = 0;
+        
+        for(int i=0; i<n; ++i){
+            if(unlock[arr[i]]<=curr){
+                unlock[arr[i]] = curr+time[arr[i]-1];
+                curr++;
+            }else{
+                curr = unlock[arr[i]];
+                unlock[arr[i]] = curr+time[arr[i]-1];
+                curr++;
+            }
+            
+        }
+        
+        return curr-1;
+    }
+}
