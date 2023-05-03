1f4cd5cf683c2425f1dc29a92c1ca2a9a11b7199
vaibhav gupta <96633424+vaibhav-gupta09@users.noreply.github.com>
2023-05-03 12:51:05 +0530

    Added solution - LeetHub

---
+++ Make Palindrome - GFG/make-palindrome.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class StringArray
+{
+    public static String[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        return s;
+    }
+
+    public static void print(String[] a)
+    {
+        for(String e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<String> a)
+    {
+        for(String e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            String[] arr = StringArray.input(br, n);
+            
+            Solution obj = new Solution();
+            boolean res = obj.makePalindrome(n, arr);
+            
+            String _result_val = (res) ? "YES" : "NO";
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static boolean makePalindrome(int n, String[] arr) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String str: arr){
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        
+        for(String str: arr){
+            StringBuilder sb = new StringBuilder(str);
+            sb.reverse();
+            if(map.getOrDefault(str, 0) != map.getOrDefault(sb.toString(), 0)) 
+               return false;
+            
+        }
+        
+        return true;
+    }
+}
+        
