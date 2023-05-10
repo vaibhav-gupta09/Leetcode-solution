cba08a426fb7d0aa96e247db898e18a738a38b8e
vaibhav gupta <96633424+vaibhav-gupta09@users.noreply.github.com>
2023-05-10 07:44:18 +0530

    Added solution - LeetHub

---
+++ Total Cuts - GFG/total-cuts.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int K;
+            K = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.totalCuts(N, K, A);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int totalCuts(int n, int k, int[] arr) {
+        int []lmax = new int[n];
+        lmax[0] = arr[0];
+        for(int i=1; i<n; ++i){
+            lmax[i] = Math.max(arr[i], lmax[i-1]);
+        }
+        
+        int rmin = arr[n-1], ans = 0;
+        for(int i=n-2; i>=0; --i){
+            if(lmax[i] + rmin >= k) ans++;
+            rmin = Math.min(rmin, arr[i]);
+        }
+        
+        return ans;
+    }
+}
+        
