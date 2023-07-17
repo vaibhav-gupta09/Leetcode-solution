e47de65c3a4199cf80d9186393776a6eb9fea73a
vaibhav gupta <96633424+vaibhav-gupta09@users.noreply.github.com>
2023-07-17 13:58:08 +0530

    Added solution - LeetHub

---
+++ Left most and right most index - GFG/left-most-and-right-most-index.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class pair  {  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            long k = Long.parseLong(br.readLine().trim());
+            
+            Solution obj = new Solution();
+            pair ans = obj.indexes(a, k);
+            if (ans.first == -1 && ans.second == -1)
+                System.out.println(-1);
+            else
+                System.out.println(ans.first+" "+ans.second);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class pair  {  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+*/
+
+class Solution {
+    
+    public pair indexes(long v[], long x)
+    {
+        int n = v.length;
+        int lo = 0, hi = n-1, mid = 0;
+        
+        while(lo <= hi){
+            mid = lo + (hi - lo)/2;
+            if(v[mid]==x) break;
+            else if(x<v[mid]) hi = mid-1;
+            else lo = mid+1;
+        }
+        
+        if(v[mid]!=x) return  new pair(-1, -1);;
+        
+        int start = mid, end = mid;
+        while(start>=0 && v[start]==x){
+            start--;
+        }
+        
+        while(end<n && v[end]==x){
+            end++;
+        }
+        
+        return new pair(start+1, end-1);
+    }
+}
