172a963d7e047dfe77ecfbadbe9a39a933cc1673
vaibhav gupta <96633424+vaibhav-gupta09@users.noreply.github.com>
2023-07-18 23:39:21 +0530

    Added solution - LeetHub

---
+++ Minimum element in a sorted and rotated array - GFG/minimum-element-in-a-sorted-and-rotated-array.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0)
+        {
+            int n = sc.nextInt();
+            int A[] = new int[n];
+            
+            for (int i = 0;i < n;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            System.out.println(sol.findMin(A, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    int findMin(int arr[], int n){
+        int pivot = 0;
+        
+        int lo = 0, hi = n-1;
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(arr[0]<=arr[mid]){
+                lo = mid+1;
+            }else{
+                pivot = mid;
+                hi = mid-1;
+            }
+        }
+        
+        return arr[pivot];
+    }
+}
