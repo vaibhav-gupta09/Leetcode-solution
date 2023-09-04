b8f6eec79195ed08e01a79141112d813cc78affb
vaibhav gupta <96633424+vaibhav-gupta09@users.noreply.github.com>
2023-09-04 11:23:52 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.java
+++ Replace O's with X's - GFG/replace-os-with-xs.java
@@ -42,54 +42,53 @@ class GFG{
 class Solution{
     static char[][] fill(int n, int m, char a[][]){
         Queue<int[]> que = new ArrayDeque<>();
-        
         for(int i=0; i<n; ++i){
-            if(a[i][0]=='O'){ 
+            if(a[i][0]=='O'){
+                a[i][0] = '1';
                 que.add(new int[]{i, 0});
-                a[i][0] = 'Y';
             }
-            if(a[i][m-1]=='O'){ 
+            if(a[i][m-1]=='O'){
+                a[i][m-1] = '1';
                 que.add(new int[]{i, m-1});
-                a[i][m-1] = 'Y';
             }
         }
         
         for(int j=0; j<m; ++j){
-            if(a[0][j]=='O'){ 
+            if(a[0][j] == 'O'){
+                a[0][j] = '1';
                 que.add(new int[]{0, j});
-                a[0][j] = 'Y';
             }
-            if(a[n-1][j]=='O'){ 
-                que.add(new int[]{n-1, j});
-                a[n-1][j] = 'Y';
+            
+            if(a[n-1][j] == 'O'){
+               a[n-1][j] = '1';
+               que.add(new int[]{n-1, j}); 
             }
         }
         
-        int[][]dir = {{0,-1}, {0,1}, {1,0}, {-1,0}};
-        while(que.size() > 0){
-            int len = que.size();
-            
-            while(len-->0){
-                int[] rem = que.remove();
-                
-                for(int k=0; k<4; ++k){
-                    int ni = rem[0] + dir[k][0];
-                    int nj = rem[1] + dir[k][1];
-                    
-                    if(ni<0 || nj<0 || ni>=n || nj>=m || a[ni][nj]!='O') continue;
-                    
-                    a[ni][nj] = 'Y';
-                    que.add(new int[]{ni, nj});
-                }
-            }
+        int[][]dir = {{0, -1}, {-1, 0}, {1, 0}, {0, 1}}; 
+        
+        while(que.size()>0){
+          int[]rem = que.remove();
+          for(int k=0; k<4; ++k){
+              int ni = rem[0] + dir[k][0];
+              int nj = rem[1] + dir[k][1];
+              
+              if(ni<0 || nj<0 || ni>=n || nj>=m || a[ni][nj]!='O') continue;
+              a[ni][nj] = '1';
+              que.add(new int[]{ni, nj}); 
+          }
         }
         
         for(int i=0; i<n; ++i){
             for(int j=0; j<m; ++j){
-                if(a[i][j]=='O') a[i][j] = 'X';
-                if(a[i][j] == 'Y') a[i][j] = 'O';
+                if(a[i][j] == '1'){
+                    a[i][j] = 'O';
+                }else{
+                    a[i][j] = 'X';
+                }
             }
         }
+        
         return a;
     }
 }
