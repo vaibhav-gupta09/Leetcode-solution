bf3b6ac0313c9ff07d8669c3bd79414376d0f038
vaibhav gupta <96633424+vaibhav-gupta09@users.noreply.github.com>
2023-10-30 14:33:36 +0530

    Added solution - LeetHub

---
+++ Sum of XOR of all pairs - GFG/sum-of-xor-of-all-pairs.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    
+		    Solution obj = new Solution();
+		    long res = obj.sumXOR(arr, sizeOfArray);
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+   
+    // Function for finding maximum and value pair
+    public long sumXOR (int arr[], int n) {
+        long result = 0;
+        
+        for(int i=0; i<32; ++i){
+            long one = 0;
+            for(int val: arr){
+                if((val & (1 << i)) != 0) one++;
+            }
+            
+            result += (one * (n - one) * (1 << i));
+        }
+        
+        return result;
+    }
+    
+    
+}
+
+
